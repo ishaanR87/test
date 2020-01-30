fbec644a629f6b282d8cae813b87610e01494646
ishaan rawat <c18459302@mytudublin.ie>
2020-01-30 09:50:02 +0000

    initial version

---
+++ src/ie/tudublin/main.java
@@ -0,0 +1,18 @@
+package ie.tudublin;
+//Purpose of package is to prevent name space conflicts//
+//.gitignore are the files you dont want to placve in your repository//
+public class Main{
+    public static void main(String[] args)
+    {
+        for(int i= 0; i< args.length; i++)
+        {
+            System.out.println(args[i]);
+        }
+
+        for(String s:args)
+        {
+            System.out.println(s);
+        }
+    }
+}
+
