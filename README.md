40b5f20fc121fc04716479b51e71c4d5e7706638
ishaan rawat <c18459302@mytudublin.ie>
2020-01-30 09:57:11 +0000

    Test branch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # test
+Git example
+
+1. Git Init command
+2. To create link with repository git remote add origin
+3. Git pull origin master 
+4. Git add . (Takes all the files from the current directory and addsthese files to a staging area)
--- src/ie/tudublin/main.java
+++ src/ie/tudublin/main.java
@@ -1,6 +1,7 @@
 package ie.tudublin;
 //Purpose of package is to prevent name space conflicts//
 //.gitignore are the files you dont want to placve in your repository//
+//changes
 public class Main{
     public static void main(String[] args)
     {
